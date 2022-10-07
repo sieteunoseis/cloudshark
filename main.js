f8a6b5c817ec2c9f27bc8088fd94cbd1c0925a76
Jeremy Worden <jeremy.worden@gmail.com>
2022-10-07 12:38:50 -0700

    Update main.js

--- main.js
+++ main.js
@@ -5,6 +5,20 @@ const path = require('path');
 
 const cloudSharkApiToken = '7cca82d38b8f2d71aac546033eb939d1';
 
+const encode = (str) =>
+encodeURIComponent(str)
+.replace(/\-/g, '%2D')
+.replace(/\_/g, '%5F')
+.replace(/\./g, '%2E')
+.replace(/\!/g, '%21')
+.replace(/\~/g, '%7E')
+.replace(/\*/g, '%2A')
+.replace(/\'/g, '%27')
+.replace(/\(/g, '%28')
+.replace(/\)/g, '%29');
+
+console.log(encode("test.cap"))
+
 (async () => {
   let output = await ciscoDime
   .getOneFile(
@@ -24,7 +38,7 @@ const cloudSharkApiToken = '7cca82d38b8f2d71aac546033eb939d1';
 
 
   fetch(
-    `https://www.cloudshark.org/api/v1/${cloudSharkApiToken}/upload`,
+    `https://www.cloudshark.org/api/v1/${cloudSharkApiToken}/upload?comments=captured+from+NOC+23&filename=test%2Ecap`,
     options
   ).then(async (response) => {
     console.log(response)
